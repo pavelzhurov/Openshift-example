c7a66382069c9e9962a1a64801e42bd33997a33e
Pavel Zhurov <pavel.zhurov@gmail.com>
2020-03-24 13:05:46 +0300

    Add hello page

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,16 @@
 from app import app
 from flask import render_template
+from flask import request
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Pavel'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html')
+
+
+@app.route('/hello')
+def hello():
+    fname = request.args.get('fname')
+    lname = request.args.get('lname')
+    return render_template('hello.html', fname=fname, lname=lname)
